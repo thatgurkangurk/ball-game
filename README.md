07992b7ca0816a95433050f7877ab814a953b5c9
Gurkan <hello@gurkz.me>
2023-11-22 18:19:01 +0100

    setup and spawn player

---
+++ README.md
@@ -0,0 +1,15 @@
+# ball game
+
+this is a game made with bevy
+
+### why?
+i wanted to learn game development and rust, and this was an easy way to kill two birds with one stone
+
+## asset sources
+| file                          | source                                       |
+|-------------------------------|----------------------------------------------|
+| audio/explosionCrunch_000.ogg | https://kenney.nl/assets/sci-fi-sounds       |
+| audio/laserLarge_000.ogg      | https://kenney.nl/assets/sci-fi-sounds       |
+| sprites/ball_blue_large.png   | https://kenney.nl/assets/rolling-ball-assets |
+| sprites/ball_red_large.png    | https://kenney.nl/assets/rolling-ball-assets |
+| sprites/star.png              | https://kenney.nl/assets/rolling-ball-assets |
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_camera)
+        .run();
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 10.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 100.0),
+        ..default()
+    });
+}
